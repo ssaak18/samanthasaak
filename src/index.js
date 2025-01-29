c3b64f50c53643db903e25d5ae495ca8bdb3ff43
Colin Balfour <75389927+ColinBalfour@users.noreply.github.com>
2025-01-28 21:34:18 -0500

    tailwind!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'; // Notice the change here
 import App from './App';
 import './index.css';
+import './output.css'; // Import the Tailwind output CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
