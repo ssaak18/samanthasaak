bf4100c29d69b02c0ef76930018234e5b4fea4c6
Samantha Saak <sammy.saak@gmail.com>
2025-01-30 01:01:29 -0500

    a

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => (
         style={{ backgroundColor: "#2e4d1a" }} 
         className="p-4 h-20 rounded-lg shadow-md flex items-center justify-center"
       >
-        <h2 className="text-2xl text-center font-bold text-white">Projects</h2>
+        <h2 className="text-3xl text-center font-bold text-white">Projects</h2>
       </div>
 
       <div className="flex flex-row justify-center items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
